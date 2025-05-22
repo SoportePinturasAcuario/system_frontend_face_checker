e61e9c93c4daa7d223b09f3096989b577b6aa72c
Jorge Jovani Diego German <soporte@pinturasacuario.com>
2025-05-22 10:42:33 -0600

    Dependency configuration

--- src/main.js
+++ src/main.js
@@ -1,14 +1,55 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
-
+import createPersistedState from 'pinia-plugin-persistedstate'
+import { es } from 'vuetify/locale';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as labs from 'vuetify/labs/components';
+import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa-svg'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+window.axios = axios
+window.axios.defaults.headers.common['Accept'] = 'application/json'
+window.axios.defaults.headers.common['Content-Type'] = 'application/json'
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.axios.defaults.withCredentials = true
+
+const pinia = createPinia()
+pinia.use(({ store }) => {
+    store.router = markRaw(router)
+})
+pinia.use(createPersistedState)
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
+library.add(fas)
+library.add(far)
 
-app.mount('#app')
+const vuetify = createVuetify({
+    locale: {
+        locale: 'es',
+        messages: { es },
+    },
+    components: {
+        ...components,
+        ...labs,
+    },
+    directives,
+    icons: {
+        defaultSet: 'fa',
+        aliases,
+        sets: {
+            fa,
+        },
+    },
+})
+app.use(pinia).use(router).use(vuetify).mount('#app')
